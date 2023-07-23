c258f82a43959eb6c9e59879fed46476320138c0
SAYYED SAMEER BASIR <107604220+sameer0208@users.noreply.github.com>
2023-07-23 16:33:26 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,27 @@
+let songs = [
+    {
+        name: 'Kesariya',
+        path: 'assets/musics/Song 1.mp3',
+        artist: 'Arijit Singh',
+        cover: 'assets/images/Cover 1.jpg'
+    },
+    {
+        name: 'Phir Aur Kya Chahiye',
+        path: 'assets/musics/Song 2.mp3',
+        artist: 'Sachin-Jigar, Arijit',
+        cover: 'assets/images/Cover 2.jpg'
+    },
+    {
+        name: 'Apna Bana Le',
+        path: 'assets/musics/Song 3.mp3',
+        artist: 'Pritam, Arijit',
+        cover: 'assets/images/cover 3.jpeg'
+    },
+    {
+        name: 'Phir Aur Kya Chahiye',
+        path: 'assets/musics/Song 4.mp3',
+        artist: 'Pritam, Arijit',
+        cover: 'assets/images/cover 4.jpg'
+    }
+    // +6 more
+]
